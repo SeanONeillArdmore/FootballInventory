6de1133ad2e42c3010c0ea538ed099cc74c53e79
Sean O'Neill <>
2022-01-29 20:18:07 +0000

    SON : add swagger

--- src/test/java/com/footballInventory/service/FootballJerseyApplicationTest.java
+++ src/test/java/com/footballInventory/service/FootballJerseyApplicationTest.java
@@ -3,7 +3,7 @@ package com.footballInventory.service;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 public class FootballJerseyApplicationTest {
 
 }
