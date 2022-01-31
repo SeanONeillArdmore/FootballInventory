e610c941b971b3e538709722ed9278638671789a
Sean O'Neill <>
2022-01-31 00:15:54 +0000

    SON : add prepolpulating files

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into jersey (id, team, size, color) values ('ee393acc-be57-4e33-bf3d-42ec97adf403', 'Man Utd', 'S', 'Red');
+insert into jersey (id, team, size, color) values ('8d752c07-b5c7-485b-a035-e104f7efa2bb', 'Liverpool', 'XL', 'Red');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE jersey (id VARCHAR(255),
+                        size VARCHAR(255),
+                        team VARCHAR(255),
+                        color VARCHAR(255));
+
+ALTER TABLE jersey ADD PRIMARY KEY (id);
